ecc3437200974cc53b5c9172d6986e1ba747d870
Dan Teixeira <danrleyt@gmail.com>
2019-06-17 19:15:16 -0300

    Changing the wifibot project

---
+++ src/api/controllers/macReader.js
@@ -0,0 +1,20 @@
+const child = require('child_process');
+var envs = require('dotenv');
+
+envs.load();
+
+function getMacs(ip, mask) {
+  var strMacs = child
+    .execSync(`sudo -S nmap -sn ${ip}-${mask} | grep MAC`)
+    .toString();
+  var macsList = strMacs.split('\n');
+
+  for (index in macsList) {
+    macsList[index] = macsList[index].replace('MAC Address: ', '');
+    macsList[index] = macsList[index].slice(0, 17);
+  }
+  macsList = macsList.slice(1, macsList.length - 1);
+  return macsList;
+}
+
+module.exports = getMacs;
